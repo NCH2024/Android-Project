ce1e5e9ff8bca4b904788dba6004643ae1997182
NCH2004 <chanhhiep.vn+GitHub@gmail.com>
2025-04-12 14:27:13 +0700

    comepleted project

--- Intent-An_GoiDien_NhanTin/app/src/main/java/com/example/intent_goi_dien_nhan_tin/GoiDien.java
+++ Intent-An_GoiDien_NhanTin/app/src/main/java/com/example/intent_goi_dien_nhan_tin/GoiDien.java
@@ -1,10 +1,11 @@
 package com.example.intent_goi_dien_nhan_tin;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,32 +13,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class GoiDien extends AppCompatActivity {
     //Khai báo các biến
-    Button btnGoi, btnNhanTin;
+    EditText edtSoDT;
+    ImageButton imgbtnGoi;
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_goi_dien);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         //Ánh xạ các biến
-        btnGoi = findViewById(R.id.btnGoi);
-        btnNhanTin = findViewById(R.id.btnNhantin);
+        edtSoDT = findViewById(R.id.edtSoDT);
+        imgbtnGoi = findViewById(R.id.imgbtnNhanTin);
+        btnBack = findViewById(R.id.btnBack);
 
         //Gắn sự kiện click cho các nút
-        btnGoi.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, GoiDien.class));
-        });
-        btnNhanTin.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, NhanTin.class));
+        btnBack.setOnClickListener(v -> {
+            finish();
         });
 
+        imgbtnGoi.setOnClickListener(v -> {
+            String soDT = edtSoDT.getText().toString();
+            if (soDT.isEmpty()) {
+                edtSoDT.setError("Vui lòng nhập số điện thoại");
+            } else {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + soDT));
+                startActivity(intent);
+            }
+        });
     }
 }
